a69d1500c340af867c2d55947e2dca3c3e2cbd7c
gbadamosi <edoardogullo@libero.it>
2021-06-22 16:38:38 +0200

    UPDATED!

--- isiapi/src/main/java/com/isi/isiapi/isinolo/HttpRequest.java
+++ isiapi/src/main/java/com/isi/isiapi/isinolo/HttpRequest.java
@@ -262,7 +262,7 @@ public class HttpRequest {
         json.addData("IsinoloBicyclesId" , prenotazione.isNoloBicyclesId);
         json.addData("CustomerId" , prenotazione.customerId);
 
-        com.isi.isiapi.isicashier.MakeHttpPost post = new com.isi.isiapi.isicashier.MakeHttpPost("addReservationApi", json.getData(), apiKey);
+        MakeHttpPost post = new MakeHttpPost("addReservationApi", json.getData(), apiKey);
 
         try {
             String response = post.execute().get();
