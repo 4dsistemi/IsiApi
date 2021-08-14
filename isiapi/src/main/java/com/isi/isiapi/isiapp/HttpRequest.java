92f226c2960f33dd27a5bd7141f405b96f550d44
Edoardo <edoardogullo@libero.it>
2021-08-14 23:20:49 +0200

    App Activation and isiapp api

--- isiapi/src/main/java/com/isi/isiapi/isiapp/HttpRequest.java
+++ isiapi/src/main/java/com/isi/isiapi/isiapp/HttpRequest.java
@@ -1,7 +1,4 @@
 package com.isi.isiapi.isiapp;
-
-import android.util.Log;
-
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.isi.isiapi.general.HttpJson;
@@ -27,8 +24,6 @@ public class HttpRequest {
         try {
             String response = post.execute().get();
 
-            Log.e("TAG", "getApplicationActive: " + response);
-
             return new Gson().fromJson(response, new TypeToken<ArrayList<ApplicationList>>(){}.getType());
 
         } catch (ExecutionException | InterruptedException e) {
@@ -50,7 +45,91 @@ public class HttpRequest {
         try {
             String response = post.execute().get();
 
-            return response.equals("ok");
+            return response.trim().equals("ok");
+
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+
+    }
+
+    public boolean addShadow(String serial, int position){
+
+        HttpJson json = new HttpJson();
+        json.addData("serial", serial);
+        json.addData("position", position);
+
+        MakeHttpPost post = new MakeHttpPost("addShadow", json.getData(), apiKey);
+
+        try {
+            String response = post.execute().get();
+
+            return response.trim().equals("ok");
+
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+
+    }
+
+    public boolean movePriority(int id, int idSecond){
+
+        HttpJson json = new HttpJson();
+        json.addData("id", id);
+        json.addData("id_second", idSecond);
+
+        MakeHttpPost post = new MakeHttpPost("movePriority", json.getData(), apiKey);
+
+        try {
+            String response = post.execute().get();
+
+            return response.trim().equals("ok");
+
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+
+    }
+
+    public boolean setPositionInMenu(int id, int position){
+
+        HttpJson json = new HttpJson();
+        json.addData("id", id);
+        json.addData("position", position);
+
+        MakeHttpPost post = new MakeHttpPost("setPositionMenu", json.getData(), apiKey);
+
+        try {
+            String response = post.execute().get();
+
+            return response.trim().equals("ok");
+
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+
+    }
+
+    public boolean updateNfc(int id, String nfc){
+
+        HttpJson json = new HttpJson();
+        json.addData("id", id);
+        json.addData("nfc", nfc);
+
+        MakeHttpPost post = new MakeHttpPost("updateNfc", json.getData(), apiKey);
+
+        try {
+            String response = post.execute().get();
+
+            return response.trim().equals("ok");
 
         } catch (ExecutionException | InterruptedException e) {
             e.printStackTrace();
