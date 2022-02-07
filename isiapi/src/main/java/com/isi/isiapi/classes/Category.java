6e583447b6447f1c49645f2fce36c7348ba3a2d9
edoardo <edoardogullo@libero.it>
2022-02-07 15:34:23 +0100

    isi V2

--- isiapi/src/main/java/com/isi/isiapi/classes/Category.java
+++ isiapi/src/main/java/com/isi/isiapi/classes/Category.java
@@ -16,9 +16,16 @@ public class Category extends LastModifiedTables{
     @SerializedName("Active")
     public int active = 1;
 
-    public Category(int id, String name, Integer color) {
+    @SerializedName("Guest")
+    public int guest = 1;
+
+    @SerializedName("Image")
+    public String image;
+
+    public Category(int id, String name, Integer color, String image) {
         this.id = id;
         this.name = name;
         this.color = color;
+        this.image = image;
     }
 }
