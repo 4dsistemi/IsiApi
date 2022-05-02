795273936e091826a5cd0d24f326349b49325659
edoardo <edoardogullo@libero.it>
2022-05-02 15:26:10 +0200

    isi V2

--- isiapi/src/main/java/com/isi/isiapi/HttpRequest.java
+++ isiapi/src/main/java/com/isi/isiapi/HttpRequest.java
@@ -51,16 +51,25 @@ import com.isi.isiapi.classes.isishop.IsicoinMovement;
 import com.isi.isiapi.classes.isishop.MyCommercialCategory;
 
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 public class HttpRequest {
 
     private final String apiKey;
+    public static String serverIpAddress = "10.99.66.2";
 
     public HttpRequest(String apiKey) {
         this.apiKey = apiKey;
+        String myIp = getIPAddress(true);
+        String substr = myIp.substring(0, myIp.lastIndexOf("."));
+        serverIpAddress = substr + ".2";
+
     }
 
     public Commercial getCommercial(){
@@ -2283,6 +2292,33 @@ public class HttpRequest {
         return false;
 
 
+    }
+
+    public String getIPAddress(boolean useIPv4) {
+        try {
+            List<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface intf : interfaces) {
+                List<InetAddress> addrs = Collections.list(intf.getInetAddresses());
+                for (InetAddress addr : addrs) {
+                    if (!addr.isLoopbackAddress()) {
+                        String sAddr = addr.getHostAddress();
+                        //boolean isIPv4 = InetAddressUtils.isIPv4Address(sAddr);
+                        boolean isIPv4 = Objects.requireNonNull(sAddr).indexOf(':')<0;
+
+                        if (useIPv4) {
+                            if (isIPv4)
+                                return sAddr;
+                        } else {
+                            if (!isIPv4) {
+                                int delim = sAddr.indexOf('%'); // drop ip6 zone suffix
+                                return delim<0 ? sAddr.toUpperCase() : sAddr.substring(0, delim).toUpperCase();
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception ignored) { } // for now eat exceptions
+        return "";
     }
 
 }
