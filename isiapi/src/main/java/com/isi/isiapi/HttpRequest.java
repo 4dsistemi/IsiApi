07bcce78fb281c3a35c5ea98e2541fa2b244b57c
edoardo <edoardogullo@libero.it>
2023-04-05 17:55:09 +0200

    inserimento ip api

--- isiapi/src/main/java/com/isi/isiapi/HttpRequest.java
+++ isiapi/src/main/java/com/isi/isiapi/HttpRequest.java
@@ -53,17 +53,13 @@ import com.isi.isiapi.classes.isiorder.IsiorderTableCategory;
 import com.isi.isiapi.classes.isiorder.IsiorderTableElement;
 import com.isi.isiapi.classes.isishop.CommercialCategoryAndMy;
 import com.isi.isiapi.classes.isishop.ExtraordinaryClosing;
-import com.isi.isiapi.classes.isishop.OpeningHours;
 import com.isi.isiapi.classes.isishop.IsicoinMovement;
 import com.isi.isiapi.classes.isishop.MyCommercialCategory;
+import com.isi.isiapi.classes.isishop.OpeningHours;
 
-import java.net.InetAddress;
-import java.net.NetworkInterface;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Date;
 import java.util.List;
-import java.util.Objects;
 
 public class HttpRequest {
 
@@ -93,6 +89,24 @@ public class HttpRequest {
             String response = post.post();
             return gson.fromJson(response, new TypeToken<ArrayList<Commercial>>() {
             }.getType());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public Commercial getCommercial(){
+
+        HttpJson json = new HttpJson();
+        json.addData("commercial", this.commercial.local_id);
+
+        MakeHttpPost post = new MakeHttpPost("getCommercial", json.getData(), "", WebControllers.isiapp);
+
+        try {
+            String response = post.post();
+            return gson.fromJson(response, Commercial.class);
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -2848,34 +2862,6 @@ public class HttpRequest {
         return false;
 
 
-    }
-
-    public String getIPAddress(boolean useIPv4) {
-        try {
-            List<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
-            for (NetworkInterface intf : interfaces) {
-                List<InetAddress> addrs = Collections.list(intf.getInetAddresses());
-                for (InetAddress addr : addrs) {
-                    if (!addr.isLoopbackAddress()) {
-                        String sAddr = addr.getHostAddress();
-                        //boolean isIPv4 = InetAddressUtils.isIPv4Address(sAddr);
-                        boolean isIPv4 = Objects.requireNonNull(sAddr).indexOf(':') < 0;
-
-                        if (useIPv4) {
-                            if (isIPv4)
-                                return sAddr;
-                        } else {
-                            if (!isIPv4) {
-                                int delim = sAddr.indexOf('%'); // drop ip6 zone suffix
-                                return delim < 0 ? sAddr.toUpperCase() : sAddr.substring(0, delim).toUpperCase();
-                            }
-                        }
-                    }
-                }
-            }
-        } catch (Exception ignored) {
-        } // for now eat exceptions
-        return "";
     }
 
 }
