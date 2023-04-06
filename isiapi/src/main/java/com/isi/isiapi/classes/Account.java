4e55d3fc78d79ece7b44caf7a0a90eab0270013b
edoardo <edoardogullo@libero.it>
2023-04-06 13:53:17 +0200

    inserimento ip api

--- isiapi/src/main/java/com/isi/isiapi/classes/Account.java
+++ isiapi/src/main/java/com/isi/isiapi/classes/Account.java
@@ -8,64 +8,44 @@ import java.util.List;
 
 public class Account {
 
-
     public int id;
-
 
     public String name;
 
-
     public String surname;
-
 
     public String email;
 
-
     public String address;
-
 
     public Float latitude;
 
-
     public Float longitude;
-
 
     public Date birthday;
 
-
     public int gender = 0;
-
 
     public String pic;
 
-
     public String phone;
-
 
     public String password;
 
-
     public String email_verification;
-
 
     public String phone_verification;
 
-
     public String general_verification;
 
-
     public String login_code = "0000";
-
-
+    
     public String nfc;
 
-
     public String secret;
-
-
+    
     public String ctzonCard;
 
-
-    public List<CtzPoint> ctzonPoints;
+    public List<CtzPoint> ctz_points;
 
 }
