422354c58cd0c921d4ac399e6ad890ce596610aa
edoardo <edoardo.gullo@4dsistemi.it>
2023-05-24 09:50:31 +0200

    inserimento ip api

--- isiapi/src/main/java/com/isi/isiapi/classes/isiorder/IsiorderGeneralInfo.java
+++ isiapi/src/main/java/com/isi/isiapi/classes/isiorder/IsiorderGeneralInfo.java
@@ -8,25 +8,20 @@ public class IsiorderGeneralInfo extends LastModifiedTables {
     
     public int id;
 
-    
     public long order_guest = 0;
 
-    
     public long reservation_service = 0;
 
-    
     public long chat_service = 0;
 
-    
     public long isidel_service = 0;
 
-    
     public int max_number_of_people = 0;
 
-    
     public float coperto = 0;
 
-    
     public boolean free_draw = false;
+
+    public boolean print_daily_number = false;
 
 }
