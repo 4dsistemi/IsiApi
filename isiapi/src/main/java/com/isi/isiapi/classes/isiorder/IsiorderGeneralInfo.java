1ba9be3da58be09f85f9635f8f73c5c1fc1ab909
edoardo <edoardo.gullo@4dsistemi.it>
2023-05-31 12:31:29 +0200

    modifica isiorderaccount to string

--- isiapi/src/main/java/com/isi/isiapi/classes/isiorder/IsiorderGeneralInfo.java
+++ isiapi/src/main/java/com/isi/isiapi/classes/isiorder/IsiorderGeneralInfo.java
@@ -7,21 +7,16 @@ public class IsiorderGeneralInfo extends LastModifiedTables {
 
     
     public int id;
-
     public long order_guest = 0;
-
     public long reservation_service = 0;
-
     public long chat_service = 0;
-
     public long isidel_service = 0;
-
     public int max_number_of_people = 0;
-
     public float coperto = 0;
-
     public boolean free_draw = false;
-
     public int print_daily_number = 0;
+    public Integer exit_2_delay;
+    public Integer exit_3_delay;
+    public Integer exit_4_delay;
 
 }
